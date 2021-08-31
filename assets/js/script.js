a94d713d1c039de7835f1304e07ad50d235b390b
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-08-31 14:12:12 -0700

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,16 +24,16 @@ function styleset(winwidth) {
    <div id="divproj" class="grid-container">
     <h4 id="p1">API Projects</h4>
         <section  class="subproject" id="main-project-container" >
-            <h4 style="color:blue; margin:5px" > Meal Planning ~ <spam style="font-size: 24px">Plan your weekly Meals! </spam>  </h4>
+            <h5 style="color:blue; margin:5px" > Meal Plan ~ <spam style="font-size: 100%">Create your Menu! </spam>  </h5>
           </section>
         <section id="Project-1" class="subproject">
-            <h5 style="color:black; font-weight:900; margin:0px 0px 0px 5px">Daily Scheduling</h5>
             <article>
-            <p>Manage work day tasks hourly!</p>
+              <h5 style="color:black; font-weight:900; margin:0px 0px 0px 5px;">Daily Scheduling</h5>
+              <p>Manage work day tasks hourly!</p>
             </article>
         </section>
         <section id="Project-2" class="subproject">
-                <br><h5 style="color:red;font-weight:900">Bike Trails</h5>
+                <h5 style="color:black;font-weight:900">Bike Trails</h5>
                 <p style="color:black;font-weight:900">Find a trail near you and Explore!</p>
         </section>
         <section id="Project-3" class="subproject">
@@ -41,10 +41,8 @@ function styleset(winwidth) {
                 <p style="color:white">Current Weather and Forecast by City</p>
         </section>
         <section id="Project-4" class="subproject">
-              
-                <h4 style="color:black">Tech Blog</h4>
-                <div id="techblog">
-                <p style="color:black; max-width: 10em">Blog about Programming Languages!</p>
+        <div id="techblog">
+                <h4 >Tech Blog</h4>
               </div>
         </section>
       </div>
